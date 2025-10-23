ee3a5a0fa40b44b94bf504fe4b2d747893d846f4
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-23 11:46:20 +0100

    function to read election data and an empty functionto beging thinking about historgram plotting

---
+++ mainCode.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+csv_in_use = "usdata.csv"
+y_axis = "Close/Last"
+# colors for plots
+colour_1 = "#2596be"
+
+
+def read_election_data():
+    '''
+    reads historicalData.csv file and parses the year column as a date and sets it as an index
+    '''
+   
+    election_df = pd.read_csv(csv_in_use, delimiter=';')
+    return election_df
+election_df = read_election_data()
+
+print(election_df)
+
+
+def plot_histogram():
+    pass
