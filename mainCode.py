611faf9bd66755d0053823afb47424722c5034e0
yourusernamenamehafybufya <hafsaabdifitah06@gmail.com>
2025-10-25 23:44:50 +0100

    managed to make a new df with totaled fractional results based on state and candidate

--- mainCode.py
+++ mainCode.py
@@ -11,15 +11,13 @@ def read_election_data():
     reads historicalData.csv file and parses the year column as a date and sets it as an index
     '''
    
-    election_df = pd.read_csv(csv_in_use, delimiter=';')
-    return election_df
-election_df = read_election_data()
+    og_df = pd.read_csv(csv_in_use, delimiter=';')
+    election_df = (
+    og_df.groupby(["state", "candidate"])["fraction_votes"]
+    .sum()
+    .reset_index()
+)
 
-print(election_df)
-
-
-def fractional_votes_state():
-    pass
 
 
 
@@ -31,11 +29,11 @@ def plot_histogram():
 
 
 #mask to get election data 
-masked_state = election_df[election_df['state'] == 'Vermont'].copy()
-print(masked_state)
+# masked_state = election_df[election_df['state'] == 'Vermont'].copy()
+# print(masked_state)
 
-masked_candidate = masked_state[masked_state['candidate'] == 'John Kasich'].copy()
-print(masked_candidate)
+# masked_candidate = masked_state[masked_state['candidate'] == 'John Kasich'].copy()
+# print(masked_candidate)
 
 
-#x axis = state and which fraction of the vote support each candidate
+# #x axis = state and which fraction of the vote support each candidate
